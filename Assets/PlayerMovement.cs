e661a1dc2197b2e1c7339526cfc77cdb831cf149
Ronald Albert <ronaldalbert1609@gmail.com>
2016-12-07 12:22:13 -0200

    Starting Fisica:PROJECT

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float w;
+    float jumpAnimAux;
+    bool isFacingRight;
+    bool walkAnimAux;
+    Animator anim;
+    Rigidbody2D rb;
+	void Start ()
+    {
+        anim = this.GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody2D>();
+	}
+    void FixedUpdate()
+    {
+        Debug.Log(rb.velocity.y);
+        w = Input.GetAxis("Horizontal");
+        walkAnimAux = (w != 0);
+        rb.velocity = new Vector2(w * speed, 0);
+        anim.SetBool("Walking", walkAnimAux);
+
+
+        jumpAnimAux = rb.velocity.y;
+        anim.SetFloat("JumpingFloat", jumpAnimAux);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * 10f);
+            anim.SetBool("Jumping", true);
+        }
+        if(isFacingRight && w > 0)
+        {
+            Flip();
+        }
+        else if(!isFacingRight && w < 0)
+        {
+            Flip();
+        }
+	}
+
+    protected void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
